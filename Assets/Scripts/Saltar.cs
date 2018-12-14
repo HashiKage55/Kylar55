e4f0699bf746962a7b5ea377f55f174d95317079
Kylar55 <Ad55am55@gmail.com>
2018-12-14 20:19:26 +0100

    Animaciones

---
+++ Assets/Scripts/Saltar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saltar : MonoBehaviour {
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Saltando", true);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetBool("Voltereta", true);
+        }
+    }
+    void End ()
+    {
+        animator.SetBool("Saltando", false);
+    }
+    void Endv()
+    {
+        animator.SetBool("Voltereta", false);
+    }
+}
