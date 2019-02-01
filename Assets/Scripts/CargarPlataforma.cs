478c4823f5be2854a3fccd954224df641278ffed
Kylar55 <Ad55am55@gmail.com>
2019-02-01 20:06:15 +0100

    Generacion Plataformas Inicio

---
+++ Assets/Scripts/CargarPlataforma.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargarPlataforma : MonoBehaviour {
+    public int sizeOfList;
+    List<GameObject> lista;
+    public int elementoSiguiente;
+
+    // Use this for initialization
+    void Start () {
+        
+        GameObject Random = GameObject.Find("ControladorMapa");
+        GeneracionPlataformas Control = Random.GetComponent<GeneracionPlataformas>();
+        lista = Control.plataformas;
+        sizeOfList = lista.Count;
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider Player)
+    {
+        Debug.Log(Random.Range(0, sizeOfList));
+        elementoSiguiente = (Random.Range(0, sizeOfList));
+        Debug.Log(lista[elementoSiguiente]);
+        
+    }
+}
--- Assets/Scripts/GeneracionPlataformas.cs
+++ Assets/Scripts/GeneracionPlataformas.cs
@@ -8,11 +8,25 @@ public class GeneracionPlataformas : ControladorBasico {
     public GameObject elementoSiguiente;
     public GameObject elementoSiguiente2;
     public Transform puntoInicio;
+    public int sizeOfList;
+
+    public List<GameObject> plataformas = new List<GameObject>();
+
+
     //public Transform puntoSiguiente;
 
     // Use this for initialization
     void Start () {
         base.Start();
+
+
+
+        sizeOfList = plataformas.Count;
+        
+
+
+        
+
 
         var plataformaInicio = Instantiate(elementoInicial, puntoInicio.position, puntoInicio.rotation);
         //Instantiate(elementoSiguiente, puntoSiguiente.position, puntoSiguiente.rotation);
@@ -21,9 +35,11 @@ public class GeneracionPlataformas : ControladorBasico {
         Transform destino2 = elemento1.gameObject.transform.Find("puntoAnclado2");
         Instantiate(elementoSiguiente2, destino2.position, destino2.rotation);
     }
-	
-	// Update is called once per frame
-	void Update () {
+    
+    
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
     public override void juegoEnPausa()
